7aa2137540d506f3f53c83a6e63d7e337ad23572
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2016-02-09 11:50:14 +0900

    First version

---
+++ asset_pricing_sdf.py
@@ -0,0 +1,169 @@
+import numpy as np
+from quantecon.markov import MarkovChain
+from mult_functional import MultiplicativeFunctional, _Result
+
+
+class AssetPricingSDF(object):
+    """
+    Class representing the asset pricing model with stochastic discount
+    factors.
+
+    Parameters
+    ----------
+    mc : MarkovChain
+        MarkovChain instance with n states representing the `X` process.
+
+    G_s : array_like(float, ndim=2)
+        Discount rate matrix. Must be of shape n x n.
+
+    G_d : array_like(float, ndim=2)
+        Growth rate matrix for the dividend. Must be of shape n x n.
+
+    d_inits : array_like(float, ndim=1), optional(default=None)
+        Array containing the initial values of the dividend, one for
+        each state. Must be of length n. If not specified, default to
+        the vector of all ones.
+
+    Attributes
+    ----------
+    mc : MarkovChain
+        See Parameters.
+
+    G_s, G_d : ndarray(float, ndim=2)
+        See Parameters.
+
+    d_inits: ndarray(float, ndim=1)
+        See Parameters.
+
+    n : scalar(int)
+        Number of the state.
+
+    P : ndarray(float, ndim=2)
+        Transition probability matrix of `mc`.
+
+    mf_s : MultiplicativeFunctional
+        MultiplicativeFunctional instance for the stochastic discount
+        factor process.
+
+    mf_d : MultiplicativeFunctional
+        MultiplicativeFunctional instance for the dividend process.
+
+    v : ndarray(float, ndim=1)
+        Dividend-price ratios.
+
+    """
+    def __init__(self, mc, G_s, G_d, d_inits=None):
+        self.mc = mc
+        self.n = mc.n
+        self.P = mc.P
+
+        self.mf_s = MultiplicativeFunctional(self.mc, G_s)
+        self.mf_d = MultiplicativeFunctional(self.mc, G_d, M_inits=d_inits)
+
+        self.P_check = self.P * self.mf_s.M_matrix
+        self.P_tilde = self.P_check * self.mf_d.M_matrix
+
+        # Solve the linear equation v = P_tilde v + P_check 1
+        A = np.identity(self.n) - self.P_tilde
+        b = self.P_check.dot(np.ones(self.n))
+        self.v = np.linalg.solve(A, b)
+
+    def simulate(self, ts_length, X_init=None,
+                 num_reps=None, random_state=None):
+        """
+        Simulate the discount factor and dividend processes.
+
+        Parameters
+        ----------
+        ts_length : scalar(int)
+            Length of each simulation.
+
+        X_init : scalar(int), optional(default=None)
+            Initial state of the `X` process. If None, the initial state
+            is randomly drawn.
+
+        num_reps : scalar(int), optional(default=None)
+            Number of repetitions of simulation.
+
+        random_state : scalar(int) or np.random.RandomState,
+                       optional(default=None)
+            Random seed (integer) or np.random.RandomState instance to
+            set the initial state of the random number generator for
+            reproducibility. If None, a randomly initialized RandomState
+            is used.
+
+        Returns
+        -------
+        res: APSDFSimulateResult
+            Simulation result represetned as a `APSDFSimulateResult`.
+            See `APSDFSimulateResult` for details. The array for each
+            attribute is of shape `(ts_length,)` if `num_reps=None`, or
+            of shape `(num_reps, ts_length)` otherwise.
+
+        """
+        X = self.mc.simulate(ts_length, init=X_init,
+                             num_reps=num_reps, random_state=random_state)
+        return self.generate_paths(X)
+
+    def generate_paths(self, X):
+        """
+        Given a simulation of the `X` process, generate sample paths of
+        the discount factor and dividend processes.
+
+        Parameters
+        ----------
+        X : array_like(int)
+            Array containing the sample path(s) of the `X` process.
+
+        Returns
+        -------
+        res: APSDFSimulateResult
+            Simulation result represetned as a `APSDFSimulateResult`.
+            See `APSDFSimulateResult` for details. The array for each
+            attribute is of the same shape as `X`.
+
+        """
+        res_s = self.mf_s.generate_paths(X)
+        res_d = self.mf_d.generate_paths(X)
+        s, s_tilde = res_s.M, res_s.M_tilde
+        d, d_tilde = res_d.M, res_d.M_tilde
+        p = d * self.v[X]
+        res = APSDFSimulateResult(X=X,
+                                  s=s,
+                                  s_tilde=s_tilde,
+                                  d=d,
+                                  d_tilde=d_tilde,
+                                  p=p)
+        return res
+
+
+class APSDFSimulateResult(_Result):
+    """
+    Contain the information about the simulation result for
+    `AssetPricingSDF`.
+
+    Attributes
+    ----------
+    s : ndarray(float, ndim=1 or 2)
+        Array containing the sample path(s) of the discount factor
+        process.
+
+    s_tilde : ndarray(float, ndim=1 or 2)
+        Array containing the sample path(s) of the multiplicative
+        martingale component of the discount factor process.
+
+    d : ndarray(float, ndim=1 or 2)
+        Array containing the sample path(s) of the dividend process.
+
+    d_tilde : ndarray(float, ndim=1 or 2)
+        Array containing the sample path(s) of the multiplicative
+        martingale component of the dividend process.
+
+    p: ndarray(float, ndim=1 or 2)
+        Array containing the sample path(s) of the dividend process.
+
+    X : ndarray(int, ndim=1 or 2)
+        Array containing the sample path(s) of the `X` process.
+
+    """
+    pass
---
+++ mult_functional.py
@@ -0,0 +1,232 @@
+import numpy as np
+
+
+class MultiplicativeFunctional(object):
+    """
+    Class representing a multiplicative functional.
+
+    Parameters
+    ----------
+    mc : MarkovChain
+        MarkovChain instance with n states representing the `X` process.
+
+    G : array_like(float, ndim=2)
+        Growth rate matrix. Must be of shape n x n.
+
+    M_inits : array_like(float, ndim=1), optional(default=None)
+        Array containing the initial values of the `M` process, one for
+        each state. Must be of length n. If not specified, default to
+        the vector of all ones.
+
+    Attributes
+    ----------
+    mc : MarkovChain
+        See Parameters.
+
+    G : ndarray(float, ndim=2)
+        See Parameters.
+
+    M_inits: ndarray(float, ndim=1)
+        See Parameters.
+
+    n : scalar(int)
+        Number of the state.
+
+    P : ndarray(float, ndim=2)
+        Transition probability matrix of `mc`.
+
+    M_matrix : ndarray(float, ndim=2)
+        Generating matrix for the `M` process.
+
+    P_tilde : ndarray(float, ndim=2)
+        Matrix representation of the M-operator, given by
+        `P * M_matrix`.
+
+    exp_eta : scalar(float)
+        Dominant eigenvalue of `P_tilde`.
+
+    eta : scalar(float)
+        Log of `exp_eta`.
+
+    e : ndarray(float, ndim=1)
+        Dominant eigenvector of `P_tilde`.
+
+    M_tilde_matrix : ndarray(float, ndim=2)
+        Generating matrix for the `M_tilde` process.
+
+    """
+    def __init__(self, mc, G, M_inits=None):
+        self.mc = mc
+        self.n = mc.n
+        self.P = mc.P
+
+        self.G = np.asarray(G)
+
+        if self.G.ndim != 2 or self.G.shape[0] != self.G.shape[1]:
+            raise ValueError('G must be a square matrix')
+
+        if self.G.shape[0] != self.n:
+            raise ValueError(
+                'order of G must be equal to the number of states'
+            )
+
+        if M_inits is None:
+            M_inits = np.ones(self.n)
+        self.M_inits = np.asarray(M_inits)
+
+        if len(self.M_inits) != self.n:
+            raise ValueError(
+                'length of M_inits must be equal to the number of states'
+            )
+
+        self.M_matrix = np.exp(self.G)
+        self.P_tilde = self.P * self.M_matrix
+
+        self.exp_eta, self.e = _solve_principal_eig(self.P_tilde)
+        self.eta = np.log(self.exp_eta)
+
+        self.M_tilde_matrix = \
+            self.M_matrix * self.e / self.e.reshape((self.n, 1)) / self.exp_eta
+
+    def simulate(self, ts_length, X_init=None,
+                 num_reps=None, random_state=None):
+        """
+        Simulate the `M` and `M_tilde` processes.
+
+        Parameters
+        ----------
+        ts_length : scalar(int)
+            Length of each simulation.
+
+        X_init : scalar(int), optional(default=None)
+            Initial state of the `X` process. If None, the initial state
+            is randomly drawn.
+
+        num_reps : scalar(int), optional(default=None)
+            Number of repetitions of simulation.
+
+        random_state : scalar(int) or np.random.RandomState,
+                       optional(default=None)
+            Random seed (integer) or np.random.RandomState instance to
+            set the initial state of the random number generator for
+            reproducibility. If None, a randomly initialized RandomState
+            is used.
+
+        Returns
+        -------
+        res: MFSimulateResult
+            Simulation result represetned as a `MFSimulateResult`. See
+            `MFSimulateResult` for details. The array for each attribute
+            is of shape `(ts_length,)` if `num_reps=None`, or of shape
+            `(num_reps, ts_length)` otherwise.
+
+        """
+        X = self.mc.simulate(ts_length, init=X_init,
+                             num_reps=num_reps, random_state=random_state)
+        return self.generate_paths(X)
+
+    def generate_paths(self, X):
+        """
+        Given a simulation of the `X` process, generate sample paths of
+        the `M` and `M_tilde` processes.
+
+        Parameters
+        ----------
+        X : array_like(int)
+            Array containing the sample path(s) of the `X` process.
+
+        Returns
+        -------
+        res: MFSimulateResult
+            Simulation result represetned as a `MFSimulateResult`. See
+            `MFSimulateResult` for details. The array for each attribute
+            is of the same shape as `X`.
+
+        """
+        X = np.asarray(X)
+        M = _generate_mult_process(X, self.M_matrix, self.M_inits)
+        M_tilde_inits = np.ones(self.n)
+        M_tilde = _generate_mult_process(X, self.M_tilde_matrix, M_tilde_inits)
+        res = MFSimulateResult(X=X,
+                               M=M,
+                               M_tilde=M_tilde)
+        return res
+
+
+def _generate_mult_process(X, mat, inits):
+    """
+    Return the array `M` given by `M[t+1]/M[t] = mat[X[t], X[t+1]]`
+    with `M[0] = inits[X[0]]`.
+
+    """
+    M = np.empty_like(X, dtype=float)
+    M[..., 0] = inits[X[..., 0]]
+    M[..., 1:] = mat[X[..., :-1], X[..., 1:]]
+    np.cumprod(M, axis=-1, out=M)
+    return M
+
+
+def _solve_principal_eig(a):
+    """
+    Solve the principal eigenvalue problem for a non-negative matrix
+    `a`.
+
+    """
+    w, v = np.linalg.eig(a)
+    idx = np.argmax(w)
+    eig_val = w[idx]
+    eig_vec = v[:, idx]
+
+    # Let eig_vec non-negative
+    sign = 0
+    i = 0
+    while sign == 0 and i < len(eig_vec):
+        sign = np.sign(eig_vec[i])
+        i += 1
+    if sign < 0:
+        eig_vec *= -1
+
+    return eig_val, eig_vec
+
+
+class _Result(dict):
+    # This is sourced from sicpy.optimize.OptimizeResult.
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+    def __repr__(self):
+        if self.keys():
+            m = max(map(len, list(self.keys()))) + 1
+            return '\n'.join([k.rjust(m) + ': ' + repr(v)
+                              for k, v in sorted(self.items())])
+        else:
+            return self.__class__.__name__ + "()"
+
+    def __dir__(self):
+        return self.keys()
+
+
+class MFSimulateResult(_Result):
+    """
+    Contain the information about the simulation result for
+    `MultiplicativeFunctional`.
+
+    Attributes
+    ----------
+    M : ndarray(float, ndim=1 or 2)
+        Array containing the sample path(s) of the `M` process.
+
+    M_tilde : ndarray(float, ndim=1 or 2)
+        Array containing the sample path(s) of the `M_tilde` process.
+
+    X : ndarray(int, ndim=1 or 2)
+        Array containing the sample path(s) of the `X` process.
+
+    """
+    pass
